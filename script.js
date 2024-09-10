049028868015ee97345a663f66817d68509b3d9e
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-10 15:45:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var isDate = function (input) {
   //   write your code here
+	return !isNaN(Data.parse(input));
 };
-
 // Do not change the code below.
-const input = prompt("Enter Date.");
+// const input = prompt("Enter Date.");
 alert(isDate(input));
